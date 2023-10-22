2d23f224062cc49373296e45e2f3860057956faf
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2023-10-23 00:44:49 +0500

    Inception

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {
+    id:"mainHeading",
+    name:"Shakir"
+}, "Hello React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
