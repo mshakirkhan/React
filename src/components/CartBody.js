22d713c28e3dd2de5e80a7b1db6ff6814ec6cc3f
Muhammad Shakir Khan <shakir.sedoc@gmail.com>
2024-07-20 03:48:37 +0500

    Feat: Add top rated product filter

--- src/components/CartBody.js
+++ src/components/CartBody.js
@@ -1,17 +1,29 @@
 import Cart from "./Cart";
 import { useState } from "react";
 import mockData from "../utils/mockData";
+console.log(mockData);
 
 const CartBody = () => {
     const [data, setData] = useState(mockData);
+
+    const topRatedHandler = () => {
+        const filteredData = data.products.filter((rt) => (rt.rating > 4));
+        setData({
+            products: filteredData
+        });
+    }
+
     return (
-        <div className='cardContainer'>
-            {
-                data.products.map((cartData) => {
-                    return <Cart key={cartData.id} cartDetails={cartData} />
-                })
-            }
-        </div>
+        <>
+            <div className="topRatedContainer"><button className="topRatedButton" onClick={topRatedHandler}>Top Rated Products</button></div>
+            <div className='cardContainer'>
+                {
+                    data.products.map((cartData) => {
+                        return <Cart key={cartData.id} cartDetails={cartData} />
+                    })
+                }
+            </div>
+        </>
     )
 }
 
